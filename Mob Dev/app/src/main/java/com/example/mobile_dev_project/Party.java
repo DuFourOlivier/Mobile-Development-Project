fb6d752589a7e237ade9c1cfa2d3298113595e6c
Olivier <Olivier.dufour@student.ap.be>
2020-11-16 11:47:09 +0100

    Added Party

---
+++ Mob Dev/app/src/main/java/com/example/mobile_dev_project/Party.java
@@ -0,0 +1,14 @@
+package com.example.mobile_dev_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Party extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_party);
+    }
+}
