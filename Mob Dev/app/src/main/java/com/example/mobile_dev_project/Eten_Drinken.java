e51cf3326635744ef5dee88391bcc0eac795db08
Olivier <Olivier.dufour@student.ap.be>
2020-11-16 11:30:02 +0100

    Added Food&Drink

---
+++ Mob Dev/app/src/main/java/com/example/mobile_dev_project/Eten_Drinken.java
@@ -0,0 +1,14 @@
+package com.example.mobile_dev_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Eten_Drinken extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eten__drinken);
+    }
+}
