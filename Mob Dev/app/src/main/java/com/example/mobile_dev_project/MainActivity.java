5b09abee70ae2e4beea4fd5a51896ec9ac6af58b
Olivier <Olivier.dufour@student.ap.be>
2020-11-23 16:48:42 +0100

    Added Navigation

--- Mob Dev/app/src/main/java/com/example/mobile_dev_project/MainActivity.java
+++ Mob Dev/app/src/main/java/com/example/mobile_dev_project/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mobile_dev_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void NavigateToNieuws(View view) {
+        Intent intent = new Intent(this,NieuwsTablad.class);
+        startActivity(intent);
+    }
+
+    public void NavigateToPlaatsen(View view) {
+        Intent intent = new Intent(this,Bekende_Plaatsen.class);
+        startActivity(intent);
+    }
+
+    public void NavigateToEten(View view) {
+        Intent intent = new Intent(this,Eten_Drinken.class);
+        startActivity(intent);
+    }
+
+    public void NavigateToParty(View view) {
+        Intent intent = new Intent(this,Party.class);
+        startActivity(intent);
     }
 }
